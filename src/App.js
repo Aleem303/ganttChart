ae573b41efa2993cbbe6de0f9df2812a8c85d578
Aleem Ullah <ualeem303@gmail.com>
2020-05-30 15:45:31 +0530

    chart

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import GoogleCharts from './components/GoogleCharts';
+
+function App(){
+  return(
+    <div>
+      <GoogleCharts/>
+    </div>
+  );
+}
+export default App;
+
